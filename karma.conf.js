52d265b1edf283dac73e46bff7c4ca2dc0aef3df
christianalfoni <christian.jorgensen@marcello.no>
2014-08-17 19:28:57 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -63,7 +63,7 @@ module.exports = function(config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         //browsers: ['Chrome', 'Firefox', 'PhantomJS'],
-        browsers: ['PhantomJS'],
+        browsers: ['Chrome'],
 
 
         // Continuous Integration mode
