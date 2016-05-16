6907e9786470a08e589b6760def81d61fe2c3c9d
Liran Nuna <liranuna@gmail.com>
2016-05-15 18:42:30 -0700

    Upgraded dependencies

--- specs/App-spec.js
+++ specs/App-spec.js
@@ -1,12 +1,13 @@
 var App = require('./../app/App.js');
-var React = require('react/addons');
-var TestUtils = React.addons.TestUtils;
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
 
 describe("App", function() {
 
   it("should render text: Hello world!", function() {
     var app = TestUtils.renderIntoDocument(React.createElement(App));
-    expect(React.findDOMNode(app).textContent).toEqual('Hello world!');
+    expect(ReactDOM.findDOMNode(app).textContent).toEqual('Hello world!');
   });
 
 });
